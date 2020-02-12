239389f6c768333576a773ff7b19317f1552121e
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2020-02-12 10:45:52 +0200

    init

--- SliderInDetails/ContentView.swift
+++ SliderInDetails/ContentView.swift
@@ -9,8 +9,14 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var sliderValue: Double = 50
+    
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            Slider(value: $sliderValue, in: 0...100)
+            Divider()
+            Text("Slider value: \(sliderValue)")
+        }.padding()
     }
 }
 
